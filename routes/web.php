309679e7c966a2459115ce98364ca7c909e426c6
Andalib Rahman Shagoto <andalibrahman369@gmail.com>
2022-11-22 20:25:16 +0600

    Made controller

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return env('CREATOR_NAME');
-    //return view('welcome');
-});
+// Route::get('/', function () {
+//     //return env('CREATOR_NAME');
+//     return view('home');
+// });
 
 //Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/register', [App\Http\Controllers\HomeController::class, 'register']);
