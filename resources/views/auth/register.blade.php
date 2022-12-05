6b502bf86152704b800712ecce12c12d082e4565
Andalib Rahman Shagoto <andalibrahman369@gmail.com>
2022-12-06 00:35:33 +0600

    Lot of work to be done, created first 2 page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,88 +1,41 @@
-{{-- @extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection --}}
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <title>Graine - Register</title>
 </head>
 <body>
-    <p>Welcome to Rg page</p>
+    <div class="w3-container w3-lime w3-center" style="height:200px;">
+        <h1>Graine</h1>
+        <p>Graine is a web application that allows you to buy or sell groceries through wide across</p>
+    </div>
+    <div class="w3-container w3-white w3-padding-large" style="height:300px;">
+        <div class="w3-container w3-lime">
+            <h4>Register</h4>
+        </div>
+          
+          <form class="w3-container" method="GET" action="/">
+            @csrf
+            <p>
+                <label>Email</label> <!-- action = to where after posting -->
+                <input class="w3-input" type="text" name="email" placeholder="Enter your email" required autocomplete="email" autofocus>
+            </p>
+            <p>
+                <label>Password</label>
+                <input class="w3-input" type="password" name="password" placeholder="Enter your password" required autocomplete="current-password">
+            </p>
+            <p>
+                <button type="submit" class="w3-button w3-lime">Register</button>
+            </p>
+          </form>
+          
+    </div>
+
+    <div class="w3-container w3-lime w3-center" style="height:100px;">
+            <i>&copy; Andalib Rahman Shagoto</i>
+    </div>
+
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <title>Graine - Homepage</title>
 </head>
 <body>
-    <h1>Welcome to the homepage</h1>
+    <div class="w3-container w3-lime w3-center" style="height:200px;">
+        <h1>Graine</h1>
+        <p>Graine is a web application that allows you to buy or sell groceries through wide across</p>
+    </div>
+    <div class="w3-container w3-white w3-padding-large" style="height:300px;">
+        <div class="w3-container w3-lime">
+            <h4>Login</h4>
+        </div>
+          
+          <form class="w3-container">
+            <p>
+                <label>Email</label>
+                <input class="w3-input" type="text">
+            </p>
+            <p>
+                <label>Password</label>
+                <input class="w3-input" type="text">
+            </p>
+          </form>
+          
+    </div>
+
+    <div class="w3-container w3-lime w3-center" style="height:100px;">
+            <i>&copy; Andalib Rahman Shagoto</i>
+    </div>
+
 </body>
 </html>
